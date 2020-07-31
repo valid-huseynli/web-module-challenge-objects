f951fdf7b9c9e63d5b19ba7c12ec233043b781eb
Your Name <you@example.com>
2020-07-30 20:28:46 -0700

    work in progress

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+  const coffee = {name: "Americano", price: 5, category: "drinks"}
+console.log(coffee)
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
